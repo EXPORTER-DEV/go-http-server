bed9dfcb8b1dc6e8c01a7119f2f32ae56f339597
Vadim <mrdrakan@Mail.ru>
2023-01-10 20:35:20 +0300

    feat: add path params example

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	server "exporter-dev/http-server/lib/core"
+	"log"
 )
 
 func main() {
@@ -17,6 +18,27 @@ func main() {
 				controller.Header.Add("Test", "test1")
 				controller.Send("Your IP: " + request.RemoteAddt)
 				return nil
+			},
+		),
+		*server.NewRoute(
+			"/index/:param/",
+			func(request *server.Request, controller *server.Controller) error {
+				controller.Status(200)
+
+				params := ""
+
+				for key, value := range request.Params {
+					params += key + ":" + value + ";"
+				}
+
+				// request.Params: map[param:1] -> URL: /index/1/
+				log.Println(request.Params)
+
+				controller.Send("Your params: " + params)
+				return nil
+			},
+			server.RouteOptions{
+				HasParams: true,
 			},
 		),
 	)
