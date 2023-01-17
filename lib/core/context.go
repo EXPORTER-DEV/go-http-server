2e6c6d93d71ad65dda9b87380619414e3895e46e
Vadim <mrdrakan@Mail.ru>
2023-01-17 19:52:58 +0300

    feat: context binding implementation, add example to main.go

--- lib/core/context.go
+++ lib/core/context.go
@@ -1,13 +1,33 @@
 package server
 
+import "reflect"
+
 type Context struct {
-	Store map[string]string
+	Store map[string]any
 }
 
-func (c *Context) Get(key string) string {
-	return c.Store[key]
+func (c *Context) Get(key string) (any, bool) {
+	value, exists := c.Store[key]
+
+	return value, exists
 }
 
-func (c *Context) Set(key string, value string) {
+func (c *Context) Set(key string, value any) {
 	c.Store[key] = value
+}
+
+func BindContext[T any](context *Context, key string) T {
+	value, exists := context.Get(key)
+
+	if !exists {
+		return *new(T)
+	}
+
+	v := reflect.ValueOf(value)
+
+	if v.IsZero() {
+		return *new(T)
+	}
+
+	return v.Interface().(T)
 }
